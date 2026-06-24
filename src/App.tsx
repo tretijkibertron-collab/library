f26e3869e582f2b241ba3d1d087de588d9f94332
kittiwake <kittiwake@mail.ru>
2026-06-24 15:09:57 +0300

    добавляет роуты

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
 import BooksPage from './pages/books/books'
 import ProfilePage from './pages/profile/profile'
 import ReadersPage from './pages/readers/readers'
+import { Route, Routes } from 'react-router-dom'
 
 
 function App() {
   return(
-    <>
-    <ProfilePage />
-    </>
+    <Routes>
+      <Route path="/" element={<BooksPage/>}/>
+      <Route path="/readers" element={<ReadersPage/>}/>
+      <Route path="/readers/:id" element={<ProfilePage/>}/>
+    </Routes>
   )
 }
 
