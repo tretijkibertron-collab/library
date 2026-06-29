ba1d233033b71384e064ac5100fb86b062d93666
kittiwake <kittiwake@mail.ru>
2026-06-29 15:31:45 +0300

    Показывает форму на экране

--- src/pages/readers/readers.tsx
+++ src/pages/readers/readers.tsx
@@ -1,8 +1,11 @@
 import ReaderList from "../../components/readers/readerList"
 import './readers.css'
 import { mockReaders } from "../../mocks/readers"
+import { useState } from "react"
+import AddReaderModal from "../../components/modals/addReaderModal"
 
 const ReadersPage = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <>
     <div className="page-header">
@@ -12,12 +15,13 @@ const ReadersPage = () => {
         Всего читателей: <strong>{mockReaders.length}</strong>
       </p>
       </div>
-      <button className="btn btn-primary" id="add-reader-btn">
+      <button className="btn btn-primary" id="add-reader-btn" onClick={()=>setShowModal(true)}>
             + Добавить читателя
       </button>
     </div>
       <ReaderList readers={mockReaders} />
-      </>
+      {showModal && <AddReaderModal />}
+    </>
   )
 }
 
