51d6b3c8bae420e00a8b4bf30768e1947f50c3d7
kittiwake <kittiwake@mail.ru>
2026-06-29 14:57:19 +0300

    добавляет кнопку создания читателя

--- src/pages/readers/readers.tsx
+++ src/pages/readers/readers.tsx
@@ -5,13 +5,19 @@ import { mockReaders } from "../../mocks/readers"
 const ReadersPage = () => {
   return (
     <>
+    <div className="page-header">
+          <div>
       <h1 className="page-title">Читатели библиотеки</h1>
       <p className="page-subtitle">
         Всего читателей: <strong>{mockReaders.length}</strong>
       </p>
-
+      </div>
+      <button className="btn btn-primary" id="add-reader-btn">
+            + Добавить читателя
+      </button>
+    </div>
       <ReaderList readers={mockReaders} />
-    </>
+      </>
   )
 }
 
